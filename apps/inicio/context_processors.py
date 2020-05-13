5e09cfa1e2fbc9d956f54cf82714642525249f70
Oscar Vogel <oscarvogel@gmail.com>
2020-05-13 08:25:24 -0300

    Ajuste de temas

--- apps/inicio/context_processors.py
+++ apps/inicio/context_processors.py
@@ -4,7 +4,11 @@ from apps.inicio.models import Paramsist
 def paramsist_processors(request):
     fb_url = Paramsist.ObtenerValor("URL_FACEBOOK")
     logo = Paramsist.ObtenerValor("LOGO")
+    whatsapp = Paramsist.ObtenerValor("WHATSAPP")
+    email = Paramsist.ObtenerValor("EMAIL")
     return {
         'fb_url': fb_url,
         'logo': logo,
+        'email': email,
+        'whatsapp': whatsapp
     }
