57e2d8b6bc8d7c046960ae4ef89c2dcef4087494
Oscar Vogel <oscarvogel@gmail.com>
2020-05-16 19:14:09 -0300

    Plantilla general con detalle de articulo

--- apps/inicio/views.py
+++ apps/inicio/views.py
@@ -1,20 +1,28 @@
 from os.path import join
 
-from django.contrib import messages
-from django.contrib.auth import authenticate
-from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render, redirect
-from django.contrib.auth import login as do_login
+from django.shortcuts import render
 # Create your views here.
 from apps.inicio.models import Paramsist, Secciones
 
 
 def inicio(request):
     template = join(Paramsist.ObtenerValor("CARPETA_TEMA"), "index.html")
-    hero = Secciones.objects.get(tipo_seccion = 'B2')
-    banner_1 = Secciones.objects.get(tipo_seccion = 'S2')
+    try:
+        hero = Secciones.objects.get(tipo_seccion = 'B2', activo=True)
+    except:
+        hero = None
+    try:
+        banner_1 = Secciones.objects.get(tipo_seccion = 'S1')
+    except:
+        banner_1 = None
+    try:
+        banner_2 = Secciones.objects.get(tipo_seccion = 'S2')
+    except:
+        banner_2 = None
+
     return render(request, template_name=template, context={
         'hero': hero,
         'banner_1': banner_1,
+        'banner_2': banner_2,
     })
 
